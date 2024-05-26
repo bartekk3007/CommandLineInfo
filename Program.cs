560a55752941927100ecfd38c167d88c0c9ec804
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-05-26 23:41:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        CommandLineInfo commandLine = new CommandLineInfo();
+
+        if (!CommandLineHandler.TryParse(args, commandLine, out string? errorMessage))
+        {
+            Console.WriteLine(errorMessage);
+            DisplayHelp();
+        }
+        if (commandLine.Help)
+        {
+            DisplayHelp();
+        }
+        else
+        {
+            if (commandLine.Priority != ProcessPriorityClass.Normal)
+            {
+                //zmienianie priorytetu watku
+            }
+        }
+    }
+
+    private static void DisplayHelp()
+    {
+        Console.WriteLine(
+            "Compress.exe / Out:< nazwa pliku > / Help \n"
+            + "/ Priority:RealRime | High |"
+            + "AboveNormal | Normal | BelowNormal | Idle");
+    }
+}
